2bd64b41cdadd8ab9f0620f6bd3207af6e31d149
Haoua01 <haoua.benaliabbo@telecom-paris.fr>
2025-04-09 13:39:08 +0200

    set server

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", async function () {
     }).addTo(map);
 
     const sqlPromise = initSqlJs({ locateFile: file => `libs/sql-wasm.wasm` });
-    const dbPromise = fetch("https://africamoneymap.fr/Africa-money-map/sqlite_data/communes_all.sqlite")
+    const dbPromise = fetch("https://github.com/Haoua01/Africa-money-map/blob/main/sqlite_data/communes_all.sqlite")
         .then(res => res.arrayBuffer())
         .then(buf => sqlPromise.then(SQL => new SQL.Database(new Uint8Array(buf))));
 
