2e127eabc2823ec9fc9b00b3a78d806f07f7a361
Haoua01 <haoua.benaliabbo@telecom-paris.fr>
2025-04-12 11:02:31 +0200

    update css for spinner z index

--- assets/script.js
+++ assets/script.js
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", async function () {
         if (document.body.contains(loadingSpinner)) {
             document.body.removeChild(loadingSpinner); // Remove spinner after delay
         }
-    }, 4000);
+    }, 3200);
     
 
 
