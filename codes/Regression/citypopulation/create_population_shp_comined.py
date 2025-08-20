b7439e41f56abdd0d0beb3e5b3c5f671dfec56c6
Haoua01 <haoua.benaliabbo@telecom-paris.fr>
2025-08-20 19:26:11 +0200

    upload population codes for scrapping

--- codes/Regression/citypopulation/create_population_shp_comined.py
+++ codes/Regression/citypopulation/create_population_shp_comined.py
@@ -1,23 +1,20 @@
 import json
 import geopandas as gpd 
+from shapely.geometry import Point
+from geopandas import GeoDataFrame
 
 import pandas as pd
 
-with open('/workspaces/Africa-money-map/data/Regression/uemoa_communes.json', 'r') as f:
+with open('/workspaces/Africa-money-map/data/Regression/citypopulation/uemoa_communes.json', 'r') as f:
     data = json.load(f)
 
-data_clean=[]
-for city in data['objs']:
-    data_clean.append({
-        'Country': 'Ghana',
-        'City': city['name'],
-        'Latitude': city['lat'],
-        'Longitude': city['lng'],
-        'Population': city['pop'][-1],
-        'Area': city['area']
-    })
+data_clean = []
+for country, info in data.items():
+    data_clean.extend(info)
+
 
 # Create a GeoDataFrame from the JSON data
 df = pd.DataFrame(data_clean)
-gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.Longitude, df.Latitude))
-gdf.to_file('/workspaces/Africa-money-map/data/Regression/communes_uemoa.shp')
+geometry = [Point(xy) for xy in zip(df["Longitude"], df["Latitude"])]
+gdf = GeoDataFrame(df, geometry=geometry)
+gdf.to_file('/workspaces/Africa-money-map/data/Regression/citypopulation/population_uemoa.shp')
